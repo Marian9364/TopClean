7a52dce904f5d0ed692e21f1cf9093884558bb5f
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2022-04-24 18:43:45 +0200

    adding pages
    added Contact Page
    added About Us page

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
 let servicesBtn = document.getElementById("servicesBtn");
+let contactBtn = document.getElementById("contactBtn");
+let aboutBtn = document.getElementById("aboutBtn");
 let picOfHome = document.getElementById("main-div");
 let servicePage = document.getElementById("services-page");
-let elementPic = document.getElementById("services-img");
+let contactPage = document.getElementById("contact-page");
+let aboutPage = document.getElementById("about-page");
+let servicesElementPic = document.getElementById("services-img");
+let contactElementPic = document.getElementById("contact-img");
+let aboutElementPic = document.getElementById("about-img");
 
-servicesBtn.addEventListener("click", onService)
+servicesBtn.addEventListener("click", onService);
+contactBtn.addEventListener("click", onContact);
+aboutBtn.addEventListener("click", onAbout);
+
 
 function onService() {
 
     picOfHome.style.display = "none";
+    contactPage.style.display = "none";
+    aboutPage.style.display = "none";
     servicePage.style.display = "block";
-    elementPic.style.opacity = 0.5;
+    servicesElementPic.style.opacity = 0.5;
+}
+
+function onContact() {
+
+    picOfHome.style.display = "none";
+    servicePage.style.display = "none";
+    aboutPage.style.display = "none";
+    contactPage.style.display = "block";
+    contactElementPic.style.opacity = 0.5;
+}
+
+function onAbout() {
+
+    picOfHome.style.display = "none";
+    servicePage.style.display = "none";
+    contactPage.style.display = "none";
+    aboutPage.style.display = "block";
+    aboutElementPic.style.opacity = 0.5;
 }
