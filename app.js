4771945df3bd6397512a4aeadce8c69855f6e4d8
Mariyan Nikolov <71733160+Marian9364@users.noreply.github.com>
2022-04-26 18:45:18 +0200

    functionality and css

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-let servicesBtn = document.getElementById("servicesBtn");
-let contactBtn = document.getElementById("contactBtn");
-let aboutBtn = document.getElementById("aboutBtn");
+let servicesBtnTop = document.querySelector("#servicesBtnTop");
+let servicesBtnFooter = document.querySelector("#servicesBtnFooter");
+let homeBtnFooter = document.querySelector("#homeBtnFooter");
+let contactBtnTop = document.querySelector("#contactBtnTop");
+let contactBtnFooter = document.querySelector("#contactBtnFooter");
+let aboutBtnTop = document.querySelector("#aboutBtnTop");
+let aboutBtnFooter = document.querySelector("#aboutBtnFooter");
 let picOfHome = document.getElementById("main-div");
 let servicePage = document.getElementById("services-page");
 let contactPage = document.getElementById("contact-page");
@@ -8,35 +12,49 @@ let aboutPage = document.getElementById("about-page");
 let servicesElementPic = document.getElementById("services-img");
 let contactElementPic = document.getElementById("contact-img");
 let aboutElementPic = document.getElementById("about-img");
+let elementPic = document.querySelector("element-pic");
 
-servicesBtn.addEventListener("click", onService);
-contactBtn.addEventListener("click", onContact);
-aboutBtn.addEventListener("click", onAbout);
+let badge = document.getElementById("badge");
 
+badge.addEventListener("click", onHome);
+homeBtnFooter.addEventListener("click", onHome);
+servicesBtnTop.addEventListener("click", onService);
+servicesBtnFooter.addEventListener("click", onService);
+contactBtnTop.addEventListener("click", onContact);
+contactBtnFooter.addEventListener("click", onContact);
+aboutBtnTop.addEventListener("click", onAbout);
+aboutBtnFooter.addEventListener("click", onAbout);
+
+function onHome() {
+  picOfHome.style.display = "block";
+  contactPage.style.display = "none";
+  aboutPage.style.display = "none";
+  servicePage.style.display = "none";
+  servicesElementPic.style.opacity = 0.0;
+  contactElementPic.style.opacity = 0.0;
+  aboutElementPic.style.opacity = 0.0;
+}
 
 function onService() {
-
-    picOfHome.style.display = "none";
-    contactPage.style.display = "none";
-    aboutPage.style.display = "none";
-    servicePage.style.display = "block";
-    servicesElementPic.style.opacity = 0.5;
+  picOfHome.style.display = "none";
+  contactPage.style.display = "none";
+  aboutPage.style.display = "none";
+  servicePage.style.display = "block";
+  servicesElementPic.style.opacity = 0.2;
 }
 
 function onContact() {
-
-    picOfHome.style.display = "none";
-    servicePage.style.display = "none";
-    aboutPage.style.display = "none";
-    contactPage.style.display = "block";
-    contactElementPic.style.opacity = 0.5;
+  picOfHome.style.display = "none";
+  servicePage.style.display = "none";
+  aboutPage.style.display = "none";
+  contactPage.style.display = "block";
+  contactElementPic.style.opacity = 0.2;
 }
 
 function onAbout() {
-
-    picOfHome.style.display = "none";
-    servicePage.style.display = "none";
-    contactPage.style.display = "none";
-    aboutPage.style.display = "block";
-    aboutElementPic.style.opacity = 0.5;
+  picOfHome.style.display = "none";
+  servicePage.style.display = "none";
+  contactPage.style.display = "none";
+  aboutPage.style.display = "block";
+  aboutElementPic.style.opacity = 0.2;
 }
